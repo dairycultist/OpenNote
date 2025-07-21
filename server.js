461e634232c835de166c5630543409376a1b62c8
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-21 19:18:59 -0400

    Simple db

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const { createServer } = require("node:http");
 
 const hostname = "localhost";
 const port = 3000;
+const secondsBetweenBackup = 60;
 
-// replacements and "injections" (the string is placed before the target string instead of replacing it)
-// inject _ with _ before _
+var serverData = {};
+
 function inject(medium, target, data) {
 
     return medium.replace(target, data + target);
@@ -20,24 +21,27 @@ const server = createServer((req, res) => {
 
             if (req.url == "/") {
 
-                var index = fs.readFileSync("html/index.html", "utf8");
+                var indexText = fs.readFileSync("html/index.html", "utf8");
 
-                for (var i = 0; i < 10; i++) {
+                for (const thread of serverData.threads) {
 
-                    var thread = fs.readFileSync("html/thread_wrapper.htm", "utf8");
+                    var threadText = fs.readFileSync("html/thread_wrapper.htm", "utf8");
 
-                    for (var j = 0; j < 3; j++) {
-                        thread = inject(thread, "<!-- POSTS -->", fs.readFileSync("html/thread_post.htm", "utf8"));
+                    threadText = threadText.replace("<!-- TITLE -->", thread.title);
+
+                    for (const post of thread.posts) {
+
+                        threadText = inject(threadText, "<!-- POSTS -->", fs.readFileSync("html/thread_post.htm", "utf8").replace("<!-- MESSAGE -->", post));
                     }
 
-                    index = inject(index, "<!-- THREADS -->", thread);
+                    indexText = inject(indexText, "<!-- THREADS -->", threadText);
                 }
 
                 // respond
                 res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-                res.end(index);
+                res.end(indexText);
 
-                // TODO handle /image /thread
+                // TODO handle /image/ID and /thread/ID
 
             } else {
 
@@ -59,6 +63,35 @@ const server = createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
+
+    // read server data json into program, or create it if it doesn't exist
+    try {
+
+        serverData = JSON.parse(fs.readFileSync("data.json", "utf8"));
+
+    } catch (err) {
+
+        serverData = {
+            "threads": [
+                {
+                    "title": "houseMD",
+                    "posts": [
+                        "I like beans",
+                        "me 2"
+                    ]
+                }
+            ]
+        };
+
+        try {
+            fs.writeFileSync("data.json", JSON.stringify(serverData));
+            console.log("Server data saved!");
+        } catch (err) {
+            console.error("Error backup up server data!");
+        }
+    }
+
+    // TODO start async thread such that every secondsBetweenBackup seconds, server data json is saved
 
     console.log(`Starting @ http://${hostname}:${port}/`);
 });
