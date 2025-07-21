c21c9505186b41f3fc903bf6922ee20aa6dd6734
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-21 19:28:58 -0400

    More db stuff

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const hostname = "localhost";
 const port = 3000;
 const secondsBetweenBackup = 60;
 
-var serverData = {};
+var db = {};
 
 function inject(medium, target, data) {
 
@@ -19,15 +19,19 @@ const server = createServer((req, res) => {
         // match endpoints
         if (req.method == "GET" || req.method == "HEAD") {
 
+            // index.html
             if (req.url == "/") {
 
                 var indexText = fs.readFileSync("html/index.html", "utf8");
 
-                for (const thread of serverData.threads) {
+                for (const threadID in db.threads) {
+
+                    var thread = db.threads[threadID];
 
                     var threadText = fs.readFileSync("html/thread_wrapper.htm", "utf8");
 
                     threadText = threadText.replace("<!-- TITLE -->", thread.title);
+                    threadText = threadText.replace("<!-- ID -->", threadID);
 
                     for (const post of thread.posts) {
 
@@ -41,20 +45,29 @@ const server = createServer((req, res) => {
                 res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
                 res.end(indexText);
 
-                // TODO handle /image/ID and /thread/ID
+            // TODO handle /image/ID and /thread/ID
 
+            // no endpoints matched
             } else {
 
                 res.writeHead(400, { "Content-Type": "text/plain" });
                 res.end("Error 400: Bad request endpoint.");
             }
+        
+        } else if (req.method == "POST") {
 
+            // process post data
+
+            // redirect to page they came from
+
+        // unimplemented request method
         } else {
 
             res.writeHead(501, { "Content-Type": "text/plain" });
             res.end("Error 501: Server has no implementation to handle " + req.method + ".");
         }
 
+    // error reading file
     } catch (err) {
 
         res.writeHead(404, { "Content-Type": "text/plain" });
@@ -67,31 +80,30 @@ server.listen(port, hostname, () => {
     // read server data json into program, or create it if it doesn't exist
     try {
 
-        serverData = JSON.parse(fs.readFileSync("data.json", "utf8"));
+        db = JSON.parse(fs.readFileSync("db.json", "utf8"));
 
     } catch (err) {
 
-        serverData = {
-            "threads": [
-                {
+        db = {
+            "threads": {
+                "0": {
                     "title": "houseMD",
                     "posts": [
                         "I like beans",
                         "me 2"
                     ]
                 }
-            ]
+            }
         };
-
-        try {
-            fs.writeFileSync("data.json", JSON.stringify(serverData));
-            console.log("Server data saved!");
-        } catch (err) {
-            console.error("Error backup up server data!");
-        }
     }
 
     // TODO start async thread such that every secondsBetweenBackup seconds, server data json is saved
+    // try {
+    //     fs.writeFileSync("db.json", JSON.stringify(db));
+    //     console.log("Database saved!");
+    // } catch (err) {
+    //     console.error("Error saving database!");
+    // }
 
     console.log(`Starting @ http://${hostname}:${port}/`);
 });
